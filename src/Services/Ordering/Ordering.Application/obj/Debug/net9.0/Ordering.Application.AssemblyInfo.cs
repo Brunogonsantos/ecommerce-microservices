f1d22295eaf165d87971ba2818b56a4ee817069c
Bruno Gonçalves dos Santos <brunogonsantos@hotmail.com>
2026-09-10 11:56:32 -0300

    Implementação do padrão de projeto Mediator para o serviço de pedidos, permitindo a separação de responsabilidades e facilitando a manutenção do código.

--- src/Services/Ordering/Ordering.Application/obj/Debug/net9.0/Ordering.Application.AssemblyInfo.cs
+++ src/Services/Ordering/Ordering.Application/obj/Debug/net9.0/Ordering.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ordering.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aff7794d30fbeaffbe2310c4916b261eaab0af12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f52e43544dc690ec22d79f124c571e0ef02370")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ordering.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ordering.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
